33ed880528573c4756e799360de0d959d402fef4
Michael Obvious <40719297+MichaelObvious@users.noreply.github.com>
2020-04-14 21:19:11 +0200

    refactoring

--- Main.hs
+++ Main.hs
@@ -10,10 +10,11 @@ data ParsedValue
 newtype Parser a = Parser { run_parser :: String -> Maybe (String, a) }
 
 char_parser :: Char -> Parser Char
-char_parser c = Parser $ \input ->
-    case input of
-        x:xs | x == c -> Just (xs, c)
-        _             -> Nothing
+char_parser c = Parser f
+    where
+        f input = case input of
+            x:xs | x == c -> Just (xs, c)
+            _             -> Nothing
 
 main :: IO ()
 main = putStr "yo"
