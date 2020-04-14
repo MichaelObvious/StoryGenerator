5e6646829e973fa7f2baccbd4b84d56e97469fff
Michael Obvious <40719297+MichaelObvious@users.noreply.github.com>
2020-04-14 21:34:39 +0200

    functor and applicative for parser

--- Main.hs
+++ Main.hs
@@ -9,6 +9,19 @@ data ParsedValue
 
 newtype Parser a = Parser { run_parser :: String -> Maybe (String, a) }
 
+instance Functor Parser where
+    fmap f (Parser p) = Parser $
+        \input -> do
+            (input', x) <- p input
+            Just (input', f x)
+
+instance Applicative Parser where
+    pure x                      = Parser $ \input -> Just (input, x)
+    (Parser p1) <*> (Parser p2) = Parser $ \input -> do
+        (input', f)  <- p1 input
+        (input'', a) <- p2 input
+        Just (input'', f a)
+
 char_parser :: Char -> Parser Char
 char_parser c = Parser f
     where
@@ -16,6 +29,9 @@ char_parser c = Parser f
             | x == c    = Just (xs, c)
             | otherwise = Nothing
         f [] = Nothing
+
+string_parser :: String -> Parser String
+string_parser str = undefined --sequenceA $ map char_parser str
 
 main :: IO ()
 main = putStr "yo"
