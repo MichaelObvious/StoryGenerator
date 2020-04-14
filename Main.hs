5627102834fae02d050f0b30e579a906ae3af466
Michael Obvious <40719297+MichaelObvious@users.noreply.github.com>
2020-04-14 21:17:26 +0200

    parsing single chars

--- Main.hs
+++ Main.hs
@@ -1,4 +1,19 @@
 module Main where
 
+data ParsedValue
+    = Text String
+    | Noun
+    | Adjective
+    | Verb
+    deriving (Show, Eq)
+
+newtype Parser a = Parser { run_parser :: String -> Maybe (String, a) }
+
+char_parser :: Char -> Parser Char
+char_parser c = Parser $ \input ->
+    case input of
+        x:xs | x == c -> Just (xs, c)
+        _             -> Nothing
+
 main :: IO ()
 main = putStr "yo"
