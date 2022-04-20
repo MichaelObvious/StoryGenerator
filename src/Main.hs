45009dfb3382d2460596362bd19667f0a397dde4
Michael Obvious <michele.palese42@outlook.it>
2022-04-20 15:19:40 +0200

    command line

--- src/Main.hs
+++ src/Main.hs
@@ -1,44 +1,22 @@
-{-# LANGUAGE FlexibleContexts  #-}
-{-# LANGUAGE OverloadedLabels  #-}
-{-# LANGUAGE OverloadedStrings #-}
-{-# LANGUAGE OverloadedLists   #-}
 module Main where
 
-import           Control.Monad                 (void)
-import           Data.Text                     (Text, pack, unpack)
+
+import System.Environment
 import           DataFileParser
-import qualified GI.Gtk                        as Gtk
-import           GI.Gtk.Declarative
-import           GI.Gtk.Declarative.App.Simple
 import           Parser
 import           System.IO.Unsafe
 import           StoryWriter
 
-data State = State { story_text :: Text, data_file :: Text }
+dataFile :: String
+dataFile = "data.txt"
 
-data Event = Closed
-          | DataFileChanged Text
-          | GenerateStory
-
-view' :: State -> AppView Gtk.Window Event
-view' s = bin Gtk.Window [ #title := "MichaelObvious's Funny Story Generator", #widthRequest := 500, #heightRequest := 250, on #deleteEvent (const (True, Closed)) ]
-    (container Gtk.Box [ #orientation := Gtk.OrientationVertical ] [ story_label, datafile_form ])
-    where story_label = BoxChild defaultBoxChildProperties { expand = True, fill = True }
-              $ widget Gtk.Label [ #label := story_text s, #halign := Gtk.AlignCenter, #valign := Gtk.AlignStart, #wrap := True ] 
-          datafile_form = container Gtk.Box
-              [ #orientation := Gtk.OrientationHorizontal ] [ datafile_form_description, datafile_input, generate_button ]
-          datafile_form_description = widget Gtk.Label
-              [ #label := pack "Data file: "]
-          datafile_input = widget Gtk.Entry
-              [ #text := data_file s, #placeholderText := "data file...", onM #changed (fmap DataFileChanged . Gtk.entryGetText) ]
-          generate_button = widget Gtk.Button
-              [ #label := "Generate", on #pressed GenerateStory ]
-
-update' :: State -> Event -> Transition State Event
-update' s e = case e of
-    DataFileChanged t -> Transition s { data_file = t } (pure Nothing)
-    GenerateStory     -> Transition s { story_text = pack $ gen_story $ unsafePerformIO (parse_file (unpack $ data_file s) data_parser) } (pure Nothing)
-    Closed            -> Exit
+printStory :: FilePath -> IO ()
+printStory path = putStrLn $ gen_story $ unsafePerformIO (parse_file path data_parser) 
 
 main :: IO ()
-main = void $ run App { view = view', update = update', inputs = [], initialState = State { story_text = "", data_file = "data.txt"} }
+main = do
+    args <- getArgs
+    if length args > 0 then
+        printStory $ head args
+    else
+        printStory dataFile
